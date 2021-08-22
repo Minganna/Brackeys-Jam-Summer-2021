e4e0686ad4bb0e90ef691309eaaea583cad43d85
unknown <s4929892@bournemouth.ac.uk>
2021-08-22 23:53:41 +0100

    end day 1 finished player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,36 @@ public class PlayerController : MonoBehaviour
     float jumpForce=5.0f;
     [SerializeField]
     float gravityScale = 5.0f;
+    [SerializeField]
+    float rotateSpeed = 10.0f;
+
+    bool StartingGrounded=false;
+
+    [SerializeField]
+    GameObject playerModel;
+
+    Animator PlayerAnimator;
 
     CharacterController charController;
+    Camera theCam;
 
     Vector3 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        charController = this.GetComponent<CharacterController>();
+        if(this.GetComponent<CharacterController>())
+        {
+            charController = this.GetComponent<CharacterController>();
+        }
+        if(Camera.main)
+        {
+            theCam = Camera.main;
+        }
+        if(playerModel.GetComponent<Animator>())
+        {
+            PlayerAnimator = playerModel.GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
@@ -31,21 +52,37 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         charController.Move((moveDirection * Time.deltaTime));
+        if(Input.GetAxisRaw("Horizontal")!=0|| Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0.0f, theCam.transform.rotation.eulerAngles.y, 0.0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0.0f, moveDirection.z));
+            if(playerModel)
+            {
+                playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+            }
+
+        }
+        PlayerAnimator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+        PlayerAnimator.SetBool("Grounded", charController.isGrounded);
+        
     }
 
     //function that control the character movements
     private void CharacterMovements()
     {
         float yStore = moveDirection.y;
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right* Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
         moveDirection *= moveSpeed;
         moveDirection.y = yStore;
 
-        if (Input.GetButtonDown("Jump"))
+        if(charController.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
         }
-
         moveDirection.y += Physics.gravity.y * Time.deltaTime*gravityScale;
 
         
